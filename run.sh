296d88101448a4e0c20622ec5534b54caeef119a
Xib3rR4dAr <systemsecur8y178@gmail.com>
2020-05-09 22:45:41 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mv .git oldSourceCode
+sed -i 's/None/All/g' /etc/apache2/apache2.conf
 service apache2 start > /dev/null 2>&1 && \
-	echo "[#] Challenge can be accessed at: http://$(hostname -I)" && \
-	tail -f /dev/null
+        echo "[#] Challenge can be accessed at: http://$(hostname -I)" && \
+        tail -f /dev/null
